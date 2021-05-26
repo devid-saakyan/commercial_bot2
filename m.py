d74bf5e872ab48b67fb40c434b87fe57d628e8de
devid-saakyan <59139074+devid-saakyan@users.noreply.github.com>
2021-05-26 04:56:14 +0300

    Update m.py

--- m.py
+++ m.py
@@ -10,7 +10,6 @@ chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument('--port=5000')
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options, port=os.environ.get("PORT", 5000))
 
 spisok_pik = ['https://www.pik.ru/search/sev-kuchino/commercial', 'https://www.pik.ru/search/kk15/commercial', 'https://www.pik.ru/search/i-les/commercial',
