a5302c473a5f1724614138796e29d912340d8c27
devid-saakyan <59139074+devid-saakyan@users.noreply.github.com>
2021-05-30 02:14:27 +0300

    Update m.py

--- m.py
+++ m.py
@@ -10,7 +10,7 @@ chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options, port=5050)
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options, port=os.environ.get("PORT", 5000))
 
 spisok_pik = ['https://www.pik.ru/search/sev-kuchino/commercial', 'https://www.pik.ru/search/kk15/commercial', 'https://www.pik.ru/search/i-les/commercial',
               'https://www.pik.ru/search/amur/commercial', 'https://www.pik.ru/search/luga/commercial', 'https://www.pik.ru/search/kuzminskyles/commercial',
@@ -60,10 +60,10 @@ if new_link != None:
     print(new_link)
     bot.send_message(719274325, 'Пополнение в ПИК\n'
                                 'Ссылка: {}'.format(new_link))
-#driver.quit()
+driver.quit()
 time.sleep(10)
 
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options, port=5001)
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options, port=os.environ.get("PORT", 5002))
 driver.get('https://www.ingrad.ru/commercial/')
 time.sleep(5)
 array_of_links = []
@@ -79,11 +79,11 @@ spisok_ingrad = array_of_links
 if new_link != None:
     bot.send_message(719274325, 'Пополнение в ИНГРАД\n'
                                 'Ссылка: {}'.format(new_link))
-#driver.quit()
+driver.quit()
 time.sleep(10)
 
 
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options, port=5002)
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options, port=os.environ.get("PORT", 5003))
 driver.get('https://samolet.ru/commercial/')
 time.sleep(5)
 array_of_links = []
@@ -99,11 +99,11 @@ spisok_samolyot = array_of_links
 if new_link != None:
     bot.send_message(719274325, 'Пополнение в Самолёт\n'
                                  'Ссылка: {}'.format(new_link))
-#driver.quit()
-time.sleep(10)
+driver.quit()
+time.sleep(5)
 
 
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options, port=5005)
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options, port=os.environ.get("PORT", 5004))
 driver.get('https://auction.samolet.ru/catalog')
 time.sleep(5)
 array_of_links = []
@@ -119,11 +119,11 @@ spisok_samolyot_auction = array_of_links
 if new_link != None:
     bot.send_message(719274325, 'Пополнение в Самолет - аукцион\n'
                                 'Ссылка: {}'.format(new_link))
-#driver.quit()
-time.sleep(10)
+driver.quit()
+time.sleep(5)
 
 
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options, port= 5003)
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options, port=os.environ.get("PORT", 5005))
 driver.get('https://fsk.ru/kommercheskaya-nedvizhimost')
 time.sleep(5)
 array_of_links = []
@@ -139,11 +139,11 @@ spisok_fsk = array_of_links
 if new_link != None:
     bot.send_message(719274325, 'Пополнение в ФСК\n'
                                 'Ссылка: {}'.format(new_link))
-#driver.quit()
-time.sleep(10)
+driver.quit()
+time.sleep(5)
 
 
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options, port=5004)
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options, port=os.environ.get("PORT", 5006))
 driver.get('https://www.lsr.ru/msk/')
 time.sleep(5)
 array_of_links = []
@@ -159,6 +159,5 @@ spisok_lsr = array_of_links
 if new_link != None:
     bot.send_message(719274325, 'Пополнение в ЛСР\n'
                                  'Ссылка: {}'.format(new_link))
-#driver.quit()
+driver.quit() 
 time.sleep(10)
-
